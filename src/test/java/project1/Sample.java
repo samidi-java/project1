a8228c4abaad39e0ea6963bca41e77bea33f6f5c
samidi-java <samidi.java@gmail.com>
2022-05-27 20:04:11 +0530

    modifed the Terst1.java

--- src/test/java/project1/Sample.java
+++ src/test/java/project1/Sample.java
@@ -3,7 +3,10 @@ package project1;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello world");
+		System.out.println("Have a good day");
+		
+		
 
 	}
 
