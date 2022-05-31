661d66f5417e6414c03f3d144572b09dff3262df
samidi-java <96601908+samidi-java@users.noreply.github.com>
2022-05-31 20:08:21 +0530

    Update Test2.java
    Test2.java fiel modified

--- src/test/java/project1/Test2.java
+++ src/test/java/project1/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to india");
 		System.out.println("Have a good day");
-
+                System.out.println("Good bye");
 	}
 
 }
